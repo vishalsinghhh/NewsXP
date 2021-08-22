f84e0e7fb77ba9be0226f6a7a4cf3c558b0da373
vishalsinghhh <vishalsingh0710vs@gmail.com>
2021-08-22 15:58:39 +0530

    remove API

--- .history/src/context_20210822155819.js
+++ .history/src/context_20210822155819.js
@@ -14,7 +14,7 @@ const AppProvider = ({ children }) => {
     setLoading(true);
     try {
       const response = await fetch(
-        `https://api.nytimes.com/svc/mostpopular/v2/viewed/${searchTerm}.json?api-key= [YOUR KEY]`
+        `https://api.nytimes.com/svc/mostpopular/v2/viewed/${searchTerm}.json?api-key= []`
       );
       const data = await response.json();
       const { results } = data;
